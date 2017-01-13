b4e982d55b59cdb78fae395336f70c854cf2ebb2
xxlxx20 <kdev2@mail.ru>
2017-01-14 01:11:45 +0300

    Delete dev1.lua

--- ppo/dev1.lua
+++
@@ -1,37 +0,0 @@
-do
-
-function run(msg, matches)
-return [[
-
-آلبہؤتہ يہعہمہل عہل مہجہمہؤعہآتہ آلسہؤبہر فہقہطہ 🚸  آلتہليہ تہصہل آلى <5K> عہضہؤ👮🏾
- 
-تہمہ صہنہعہ هہذآ آلبہؤتہ بہؤآسہطہهہ آلمہطہؤريہنہ (كہرآر + غہيہثہ)
-         admin◾️ @llX8Xll
-         admin◾️ @llllll_lllll
-           ๒๏t◾️ @llX8Xllbot
-
-            سہؤرسہ كہيہبہر_نہيہؤ آلجہديہد KEEPER_NEW تہمہ صہنہعہهہ بہؤآسہہطہهہ آلمہطہؤريہيہنہ 
---------------
-admin◾️ @llX8Xll
-admin◾️ @llllll_lllll
-بہؤتہ تہؤآصہل آلمہحہظہؤريہنہ
-  ๒๏t◾️ @llX8Xllbot
-
-  قہنہآة آلسہؤرسہ
- ςђ◾️@DEV_KEEPER_NEW
-                     
- Programmable bot : @llX8Xllbot
-                     
- Bot continues coder : @DEV_KEEPER_NEW
-]]
-end
-
-return {
-description = "Shows bot q", 
-usage = "spam Shows bot q",
-patterns = {
-"المطور"
-},
-run = run 
-}
-end
